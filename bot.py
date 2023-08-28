087c7534fefa4a0f2c17fc059f4a4afcbe7dc4e5
hydrol0x <mrjacobry@gmail.com>
2023-08-27 21:23:02 -0400

    set channel

--- bot.py
+++ bot.py
@@ -10,6 +10,7 @@ import discord
 
 bot = discord.Bot()
 bot.ghost_ping_user = ""
+bot.pingchannel = None
 bot.time_delay = 0.5 
 
 responses = ["PING", "I hate you", "Loser", "L", "Who asked?", "When did I ask?", "OK", "LOL", "ping"]
@@ -26,6 +27,10 @@ async def on_message(message):
         if str(message.author.id) == str(bot.ghost_ping_user):
             mention_string = message.author.mention
             response= responses[random.randint(0, len(responses)-1)]
+            if bot.pingchannel:
+                channel = bot.get_channel(bot.pingchannel)
+                await channel.send(response + " "+  mention_string, delete_after=bot.time_delay)
+                return
             await message.channel.send(response + " "+  mention_string, delete_after=bot.time_delay)
 
 @bot.slash_command()
@@ -45,6 +50,12 @@ async def setdelay(ctx, time_delay: float):
     time_delay = max(time_delay, 0)
     bot.time_delay = min(time_delay, 10) 
     await ctx.respond(f"Set time delay for ghost ping deletion to {bot.time_delay}!")
+
+@bot.slash_command()
+async def setpingchannel(ctx, pingchannel_id):
+    await ctx.respond(f"Set channel where ping will happen to {pingchannel_id}!")
+    
+    bot.pingchannel = pingchannel_id 
 
 
 bot.run(TOKEN)
